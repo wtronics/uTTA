94568d4145c90071bca53a2014b5409e291c9976
wtronics <w.m.d.koch@googlemail.com>
2024-08-22 11:47:11 +0200

    Update uTTA_Deconvolution_LucyRichardson_file.py
    Tried to improve deconvolution

--- 050_Phyton_Scripts/uTTA/uTTA_Deconvolution_LucyRichardson_file.py
+++ 050_Phyton_Scripts/uTTA/uTTA_Deconvolution_LucyRichardson_file.py
@@ -7,31 +7,19 @@ from skimage import color, data, restoration
 import os
 from numpy import genfromtxt
 from tkinter import filedialog as fd
-
-StartFilePath = os.path.realpath(__file__)
-
-def select_file():
-    filetypes = (('T3I Measurement Files', '*.t3i'), ('All files', '*.*'))
-
-    filename = fd.askopenfilename(
-        title='Open a T3R-File',
-        initialdir=StartFilePath,
-        filetypes=filetypes
-    )
-    return filename
+import uTTA_data_import
 
 
-FileNam = select_file()
+FileNam = uTTA_data_import.select_file('Open a t3i-File', (('T3I Measurement Files', '*.t3i'), ('All files', '*.*')))
 
 DataFile = os.path.basename(FileNam).split('/')[-1]
 DataFileNoExt = DataFile.replace('.t3i', '')
 FilePath = os.path.dirname(FileNam)
 
 NoOfMonitors = 2
-SamplesPerDecade = 100
-
-Phi0 = 0.9
-rho = 0.1
+SamplesPerDecade = 20
+# deconv_zShift = 1.144472988
+deconv_zShift = 1.138+0.017091077755321222
 
 start = time.time()
 
@@ -74,40 +62,36 @@ axs[0, 0].set_title("Input Zth Curve")
 axs[0, 0].set_ylabel('Zth / [K/W]')
 axs[0, 0].set_xlabel('Time / [ln(s)]')
 
-
 axs[0, 1].plot(z[:-1], dadz, label="da/dz")  # Plot some data on the axes.
 axs[0, 1].set_title("Interpolated and differentiated Zth Curve")
 axs[0, 1].set_ylabel('Derivative of Zth / [K/W/ln(s)')
 axs[0, 1].set_xlabel('Time / [ln(s)]')
 
-
 print("z-Base Timestep: " + str(z[2]-z[1]))
 
-
 Rth_Stat = np.mean(a_z[int(0.98*len(a_z)):-1])
-IterationSteps = np.array([1000], dtype=int)
+IterationSteps = np.array([1000, 250], dtype=int)
 
 for Step in IterationSteps:
     deconvolved = restoration.richardson_lucy(dadz, w_z, num_iter=Step)*sum_dadz
 
-    axs[1, 0].plot(z[:-1], deconvolved, label="Deconvolved with " + str(Step) + " Iterations")  # Plot some data on the axes.
+    axs[1, 0].plot(z[:-1]+deconv_zShift, deconvolved, label="Deconvolved with " + str(Step) + " Iterations")  # Plot some data on the axes.
 
     PeakSum = np.sum(deconvolved)*(z[2]-z[1])
     print("Total deconvolved Rth {Rth}K/W vs. Original Rth {RthOrig}K/W".format(Rth=PeakSum, RthOrig=Rth_Stat))
 
     dadz_conv = (np.convolve(deconvolved, w_z, "same") / np.sum(deconvolved)) * (z[2]-z[1])
     Zth_Conv = np.cumsum(dadz_conv) * Rth_Stat + a_z[0]
-    StructureFunc = np.exp(np.cumsum(deconvolved) * np.diff(z))
 
     axs[0, 1].plot(z, dadz_conv, label="da/dz " + str(Step) + " Iterations")  # Plot some data on the axes.
     axs[1, 1].plot(z[:-1], dadz-dadz_conv[:-1], label="Delta da/dz " + str(Step) + " Iterations")  # Plot some data on the axes.
     axs[0, 0].loglog(np.exp(z), Zth_Conv, label="Reconstructed Zth " + str(Step) + " Iterations")  # Plot some data on the axes.
     axs[2, 0].semilogx(np.exp(z), Zth_Conv-a_z, label="Delta Zth " + str(Step) + " Iterations")  # Plot some data on the axes.
 
-
 axs[0, 0].legend()
 axs[0, 0].grid(which='both')
 
+axs[0, 1].set_title("Interpolated time constant spectrum")
 axs[1, 0].legend()
 axs[1, 0].grid(which='both')
 
@@ -119,6 +103,9 @@ axs[0, 1].legend()
 
 axs[1, 1].grid(which='both')
 axs[1, 1].legend()
+
+# axs[2, 1].legend()
+# axs[2, 1].grid(which='both')
 
 end = time.time()
 print("Execution Time: {time:.3f}s".format(time=end - start))
