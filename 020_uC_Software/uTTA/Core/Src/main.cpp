9f6d06458dc674654763f270afe0581ac2306c1f
wtronics <w.m.d.koch@googlemail.com>
2024-11-10 13:03:36 +0100

    Update to SW 2.0 functions

--- 020_uC_Software/uTTA/Core/Src/main.cpp
+++ 020_uC_Software/uTTA/Core/Src/main.cpp
@@ -17,19 +17,11 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
-#include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-#include "SCPI_server.h"
-#include "rtc.h"
-#include "build_defs.h"
+#include <main.hpp>
 #include "Vrekrer_scpi_parser.hpp"
-#include "ErrorCodes.h"
 #include "MAX6675.h"
-
+#include "SCPI_server.h"
+#include "dev_cal.h"
 
 /* Private function prototypes -----------------------------------------------*/
 
@@ -38,18 +30,16 @@ W25QXX_HandleTypeDef w25qxx;
 lfs_file_t file;
 lfs_t littlefs;
 
-RTC_TimeTypeDef sTime;
-RTC_DateTypeDef sDate;
-
 char LogWriteBfr[TX_BFR_SIZE];
 uint32_t ADC_Buffer12[ADC_BFR_BLOCKS*ADC_BFR_SIZE] __attribute__((aligned (4)));
 uint32_t ADC_Buffer34[ADC_BFR_BLOCKS*ADC_BFR_SIZE] __attribute__((aligned (4)));
 
 volatile uint8_t ADC_PGA_Setting[ADC_BFR_BLOCKS];
-volatile uint32_t ADC_TotalBlocks = 0;
-volatile uint32_t ADC_CoolingStartBlock = 0;
-extern PGA PGA_Gains;
-extern volatile Timing SamplingTiming;
+volatile int32_t ADC_TotalBlocks = 0;
+volatile int32_t ADC_CoolingStartBlock = 0;
+PGA_t PGA_Gains = {PGA_DEF_GAIN, PGA_DEF_GAIN, PGA_DEF_GAIN};
+volatile Timing_t SamplingTiming = {ADC_DEF_SAMPLETIME, 0.0f, ADC_MAX_MULTIPLIER, ADC_MAX_SAMPLERATE << ADC_MAX_MULTIPLIER, ADC_MAX_MULTIPLIER, MEAS_DEF_PREHEATING_TIME, MEAS_DEF_HEATING_TIME, MEAS_DEF_COOLING_TIME, CAL_DEF_SAMPLE_TIME};
+
 
 uint32_t Meas_RunTime = 0;
 uint32_t Meas_StartTime = 0;
@@ -59,11 +49,12 @@ int GlobalWriteErrorFlag = 0;
 volatile MeasurementStates_t FlagMeasurementState = Meas_State_Idle;
 
 uint8_t OperatingMode = MODE_NORMAL;
-char DUT_Name[MAX_DUT_NAME_LENGTH] = {"DUT.t3r"};
+char DUT_Name[MAX_DUT_NAME_LENGTH] = {"DUT.umf"};
 
 CH_Def Channels[4] = { 	{"Driver"  , 0, -2600, 0},
 						{"Monitor1", 0, -2600, 0},
-						{"Monitor2", 0, -2600, 0}};
+						{"Monitor2", 0, -2600, 0},
+						{"Spare"   , 0, -2600, 0}};
 
 
 char TxBuffer[TX_BFR_SIZE];
@@ -97,7 +88,7 @@ int main(void)
   SysTick_Config(SystemCoreClock/1000);
 
   //LL_SYSTICK_EnableIT();
-  LL_mDelay(1000);
+  LL_mDelay(500);
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
@@ -118,7 +109,7 @@ int main(void)
   if(StartupDate.RTC_Year == 0){
 	  RTC_write_date_fmt(BUILD_YEAR ,BUILD_MONTH, BUILD_DAY, 1);
 	  RTC_write_time_fmt(BUILD_HOUR, BUILD_MIN, BUILD_SEC);
-	  UART_printf("Reinitialized RTC\n");
+	  INIT_DBG("Reinitialized RTC\n");
   }
 
   Enable_ADC_Clock();
@@ -126,26 +117,34 @@ int main(void)
   LL_GPIO_ResetOutputPin(PSU_EN_DO_GPIO_Port, PSU_EN_DO_Pin);
   LL_GPIO_ResetOutputPin(PWSTG_EN_DO_GPIO_Port, PWSTG_EN_DO_Pin);
 
-  my_instrument.hash_magic_number = 47; //Default value = 37
-  my_instrument.hash_magic_offset = 13;  //Default value = 7
+  my_instrument.hash_magic_number = 53; //Default value = 37
+  my_instrument.hash_magic_offset = 3;  //Default value = 7
   Init_SCPI_server();
 
-  my_instrument.PrintDebugInfo(USART2);
+  my_instrument.PrintDebugInfo(USART2, 0);
 
-  UART_printf("Init SD Card!\n");
+  INIT_DBG("Init SD Card!\n");
 
-  if (w25qxx_init(&w25qxx, SPI3, SD_SPI_CS_GPIO_Port, SD_SPI_CS_Pin) == W25QXX_Ok) {
-	  UART_printf("W25QXX successfully initialized\n");
-	  UART_printf("Manufacturer       = 0x%2x\n", w25qxx.manufacturer_id);
-	  UART_printf("Device             = 0x%4x\n", w25qxx.device_id);
-	  UART_printf("Block size         = 0x%04x (%lu)\n", w25qxx.block_size, w25qxx.block_size);
-	  UART_printf("Block count        = 0x%04x (%lu)\n", w25qxx.block_count, w25qxx.block_count);
-	  UART_printf("Total size (in kB) = 0x%04x (%lu)\n", (w25qxx.block_count * w25qxx.block_size) / 1024, (w25qxx.block_count * w25qxx.block_size) / 1024);
+  if (w25qxx_init(&w25qxx, SPI3, FLASH_SPI_CS_GPIO_Port, FLASH_SPI_CS_Pin) == W25QXX_Ok) {
+	  INIT_DBG("W25QXX successfully initialized\n");
+	  INIT_DBG("Manufacturer       = 0x%2x\n", w25qxx.manufacturer_id);
+	  INIT_DBG("Device             = 0x%4x\n", w25qxx.device_id);
+	  INIT_DBG("Block size         = 0x%04x (%lu)\n", w25qxx.block_size, w25qxx.block_size);
+	  INIT_DBG("Block count        = 0x%04x (%lu)\n", w25qxx.block_count, w25qxx.block_count);
+	  INIT_DBG("Total size (in kB) = 0x%04x (%lu)\n", (w25qxx.block_count * w25qxx.block_size) / 1024, (w25qxx.block_count * w25qxx.block_size) / 1024);
   }
 
   EvalLFSError(w25qxx_littlefs_init(&w25qxx));
 
-  UART_printf("Init completed!\n");
+  Read_CalibrationFromFlash(&littlefs, &file);
+
+  INIT_DBG("DAC Initialisation!\n");
+
+  AD56x4_Init(&ChannelCalibValues[DAC_Ch_ISEN]);
+  AD56x4_SetDefaultOutput();
+
+
+  INIT_DBG("Init completed!\n");
   UART_printf("RTC Time: ");
   print_RTC_DateTime(0,0);
   UART_printf("\n");
@@ -157,7 +156,12 @@ int main(void)
 
 	  char* message = my_instrument.GetMessage(USART2, RxBuffer_Stat,"\n");
 	  if (message != NULL) {
-		  my_instrument.Execute(message, USART2);
+		  if(my_instrument.comm_mode == CommMode_SCPI){
+			  my_instrument.Execute(message, USART2);
+		  }else{
+			  FileUpload_SerialToFile(message);
+		  }
+
 	  }
 	  DoMeasurement();
 
@@ -185,11 +189,12 @@ void DoMeasurement(void)
 	static uint32_t MeasurementStartTime;		// holds the start time of the measurement
 	static uint32_t MeasurementNextStepTime;	// the time when the next timestep will be done (roughly)
 	static uint8_t ADC_BufferReadBlock;
-	static uint32_t Log_WrittenBlocks;
+	static int32_t Log_WrittenBlocks;
 	static uint32_t NextOutput;
 	union ADC_conv adc_val;
 	int16_t ADC_BFR_idx;
 	bool PrintLastADC=false;
+	RTC_TimeTypeDef Time;
 
 
 	int LFS_ret = 0;
@@ -211,7 +216,7 @@ void DoMeasurement(void)
 			break;
 		}
 
-		WriteFileHeaderToFile();
+		WriteFileHeaderToFile(&littlefs, &file);
 
 		LL_GPIO_SetOutputPin(PSU_EN_DO_GPIO_Port, PSU_EN_DO_Pin); // Enable the output of the Main Power Supply
 
@@ -263,8 +268,14 @@ void DoMeasurement(void)
 
 		if(GetTick()< MeasurementNextStepTime) break;
 
-		MeasurementNextStepTime += SamplingTiming.HeatingTime;
-		FlagMeasurementState = Meas_State_PrepHeating;
+		if( SamplingTiming.HeatingTime > 0){		// check if this is a real measurement or just a calibration run
+			MeasurementNextStepTime += SamplingTiming.HeatingTime;
+			FlagMeasurementState = Meas_State_PrepHeating;
+		}else		// Just a calibration measurement
+		{
+			FlagMeasurementState = Meas_State_Deinit;
+			ADC_CoolingStartBlock = -1;
+		}
 		break;
 	case Meas_State_Heating:
 
@@ -301,7 +312,7 @@ void DoMeasurement(void)
 			Log_WrittenBlocks++;
 		}while(Log_WrittenBlocks <= ADC_TotalBlocks);
 
-		WriteFileFooterToFile();
+		WriteFileFooterToFile(&littlefs, &file, ADC_CoolingStartBlock, ADC_TotalBlocks);
 		EvalLFSError(lfs_file_sync(&littlefs, &file));
 		EvalLFSError(lfs_file_close(&littlefs, &file));
 
@@ -311,7 +322,6 @@ void DoMeasurement(void)
 
 		ADC_CoolingStartBlock =0;
 		Log_WrittenBlocks = 0;
-		RTC_TimeTypeDef Time;
 		RTC_read_time(&Time);
 		UART_printf("Measurement completed! ");
 		UART_printf("%02u:%02u:%02u\n",Time.RTC_Hours,Time.RTC_Minutes,Time.RTC_Seconds);
@@ -325,9 +335,6 @@ void DoMeasurement(void)
 		LL_GPIO_SetOutputPin(PWSTG_PWR_EN_DO_GPIO_Port, PWSTG_PWR_EN_DO_Pin);		// Enable the gate driver supply
 		LL_GPIO_SetOutputPin(PSU_EN_DO_GPIO_Port, PSU_EN_DO_Pin);					// Enable the output of the Main Power Supply
 
-		EvalLFSError(lfs_mkdir(&littlefs,"Cal"));
-		EvalLFSError(lfs_file_open(&littlefs, &file, "Cal/CAL.txt", LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC));
-		WriteFileHeaderToFile();
 		Setup_TIM1(TIM_Mode_Cal);
 
 		SamplingTiming.SetMultiplier = SamplingTiming.MaxTimeMultiplier;
@@ -359,8 +366,21 @@ void DoMeasurement(void)
 		LL_GPIO_ResetOutputPin(PSU_EN_DO_GPIO_Port, PSU_EN_DO_Pin);			// Disable the power supply to minimize parasitic effects through the hot powerstage
 		LL_GPIO_ResetOutputPin(PWSTG_PWR_EN_DO_GPIO_Port, PWSTG_PWR_EN_DO_Pin); // Disable the gate driver power supply
 
-		FlagMeasurementState = Meas_State_CloseLog;	 // Set the step for the StateMachine to the next step
+		FlagMeasurementState = Cal_State_Exit;	 // Set the step for the StateMachine to the next step
 
+		break;
+	case Cal_State_Exit:
+
+		ADC_CoolingStartBlock =0;
+		Log_WrittenBlocks = 0;
+
+		RTC_read_time(&Time);
+		UART_printf("Measurement completed! ");
+		UART_printf("%02u:%02u:%02u\n",Time.RTC_Hours,Time.RTC_Minutes,Time.RTC_Seconds);
+
+		ErrorsOutput();
+
+		FlagMeasurementState = Meas_State_Idle;
 		break;
 	default:
 
@@ -369,7 +389,7 @@ void DoMeasurement(void)
 
 	// Data logging loop
 	if (FlagMeasurementState){
-		if ((ADC_TotalBlocks>0) && (Log_WrittenBlocks < ADC_TotalBlocks) ){
+		if ((ADC_TotalBlocks>0) && (Log_WrittenBlocks < ADC_TotalBlocks) && (FlagMeasurementState < Meas_State_CloseLog) ){
 			LD2_GPIO_Port->BSRR = LD2_Pin;
 
 			ADC_BufferReadBlock = Log_WrittenBlocks % ADC_BFR_BLOCKS;	// Calculate the virtual Block number
@@ -381,14 +401,17 @@ void DoMeasurement(void)
 				Log_WrittenBlocks++;
 			}
 			else{
-				ADC_TotalBlocks--;	// Some botch to fix the mysterious behaviour of the 100 samples offset
+				ADC_TotalBlocks--;	// Some botch to fix the mysterious behaviour of 100 samples offset
 			}
 
 			LD2_GPIO_Port->BRR = LD2_Pin;
 		}
 
 		if(GetTick() > NextOutput){		// Measurement is running
-			WriteTemperaturesToFile();
+			if(OperatingMode != MODE_CALIBRATION){
+				WriteTemperaturesToFile();
+			}
+
 
 			if((GetTick() - NextOutput) < MEASURE_DATA_UPDATE_TIME){
 				NextOutput += MEASURE_DATA_UPDATE_TIME;
@@ -426,86 +449,6 @@ void DoMeasurement(void)
 	}
 }
 
-/**************************************************************************/
-/*!
-    @brief  Writes the file header with all information to the file
-    @param none
-    @returns none
-*/
-/**************************************************************************/
-int8_t WriteFileHeaderToFile(void){
-	int8_t LFS_ret=0;
-	/* Writing text */
-	RTC_TimeTypeDef Time;
-	RTC_DateTypeDef Date;
-	RTC_read_date(&Date);
-	RTC_read_time(&Time);
-	sprintf(LogWriteBfr,"FileVersion;" T3R_FILEVERSION "\n");
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"CH1 Name;%s;%ld;%ld;%ld\n",Channels[0].CH_Name,Channels[0].CH_Offs,Channels[0].CH_LinGain,Channels[0].CH_QuadGain);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"CH2 Name;%s;%ld;%ld;%ld\n",Channels[1].CH_Name,Channels[1].CH_Offs,Channels[1].CH_LinGain,Channels[1].CH_QuadGain);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"CH3 Name;%s;%ld;%ld;%ld\n",Channels[2].CH_Name,Channels[2].CH_Offs,Channels[2].CH_LinGain,Channels[2].CH_QuadGain);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"StartTime;%02d:%02d:%02d\n",Time.RTC_Hours,Time.RTC_Minutes,Time.RTC_Seconds);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"StartDate;%02d.%02d.%04d\n",Date.RTC_Date, Date.RTC_Month, ((uint16_t)Date.RTC_Year)+2000);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"Tsamp,fast;%lu\n",SamplingTiming.FastSampleTime);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"Tsamp,low;%lu\n",SamplingTiming.FastSampleTime<<SamplingTiming.MaxTimeMultiplier);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"Samples/Decade;%u\n",ADC_BFR_SIZE);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"Max. Divider;%lu\n",SamplingTiming.MaxTimeMultiplier);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"T_Preheat;%lu\n",SamplingTiming.PreHeatingTime/1000);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"T_Heat;%lu\n",SamplingTiming.HeatingTime/1000);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	sprintf(LogWriteBfr,"T_Cool;%lu\n",SamplingTiming.CoolingTime/1000);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, "ADC1;ADC2;ADC3;ADC4\n"));
-
-	return LFS_ret;
-}
-
-
-/**************************************************************************/
-/*!
-    @brief  Writes the file header with all information to the file
-    @param none
-    @returns none
-*/
-/**************************************************************************/
-int8_t WriteFileFooterToFile(void){
-	RTC_TimeTypeDef Time;
-	int8_t LFS_ret = 0;
-
-	sprintf(LogWriteBfr,"Cooling Start Block;%lu\nTotal Blocks;%lu\n", ADC_CoolingStartBlock, ADC_TotalBlocks);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-
-	RTC_read_time(&Time);
-
-	sprintf(LogWriteBfr,"Finished;%02u:%02u:%02u\nEnd of Log", Time.RTC_Hours,Time.RTC_Minutes,Time.RTC_Seconds);
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-	return LFS_ret;
-}
-
 
 /**************************************************************************/
 /*!
@@ -531,17 +474,17 @@ uint8_t WriteBlockToFile(uint8_t ReadBlock, uint32_t TotalWrittenBlocks)
 	if(DMA_CurrentPos > Block_End || DMA_CurrentPos < Block_Start)	// check if the DMA counter is out of the way...
 	{
 		// format every sample into a line and print it onto the SD card. (single utoa's are ~50% faster than one sprintf)
-		LFS_ret = LFS_WRITE_STRING(littlefs, file, "#B;");	// Block Number
+		LFS_ret = LFS_WRITE_STRING(&littlefs, &file, "#B;");	// Block Number
 
 		utoa(TotalWrittenBlocks,LogWriteBfr,10);
-		LFS_ret = LFS_WRITE_STRING(littlefs, file, LogWriteBfr);
+		LFS_ret = LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr);
 
-		LFS_ret = LFS_WRITE_STRING(littlefs, file, ";\n");
-		LFS_ret = LFS_WRITE_STRING(littlefs, file, "#P;");	// PGA Setting
+		LFS_ret = LFS_WRITE_STRING(&littlefs, &file, ";\n");
+		LFS_ret = LFS_WRITE_STRING(&littlefs, &file, "#P;");	// PGA Setting
 
 		utoa(ADC_PGA_Setting[ReadBlock],LogWriteBfr,10);
-		LFS_ret = LFS_WRITE_STRING(littlefs, file, LogWriteBfr);
-		LFS_ret = LFS_WRITE_STRING(littlefs, file, ";\n");
+		LFS_ret = LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr);
+		LFS_ret = LFS_WRITE_STRING(&littlefs, &file, ";\n");
 
 		union ADC_conv ADC_RegVal;
 		for(ADC_Idx = Block_Start; ADC_Idx < Block_End; ADC_Idx++){
@@ -549,21 +492,21 @@ uint8_t WriteBlockToFile(uint8_t ReadBlock, uint32_t TotalWrittenBlocks)
 			ADC_RegVal.ADC_Reg = ADC_Buffer12[ADC_Idx];
 
 			utoa(ADC_RegVal.ADC_Val[0],LogWriteBfr,10);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, LogWriteBfr);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, ";");
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr);
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, ";");
 			utoa(ADC_RegVal.ADC_Val[1],LogWriteBfr,10);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, LogWriteBfr);
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr);
 
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, ";");
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, ";");
 
 			ADC_RegVal.ADC_Reg = ADC_Buffer34[ADC_Idx];
 
 			utoa(ADC_RegVal.ADC_Val[0],LogWriteBfr,10);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, LogWriteBfr);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, ";");
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr);
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, ";");
 			utoa(ADC_RegVal.ADC_Val[1],LogWriteBfr,10);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, LogWriteBfr);
-			LFS_ret = LFS_WRITE_STRING(littlefs, file, ";\n");
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr);
+			LFS_ret = LFS_WRITE_STRING(&littlefs, &file, ";\n");
 		}
 		if(LFS_ret <0){
 			GlobalWriteErrorFlag =1;
@@ -585,14 +528,14 @@ uint8_t WriteTemperaturesToFile(void){
 
 	int LFS_ret = 0;
 
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, "#T;"));	// Thermocouples
+	EvalLFSError(LFS_WRITE_STRING(&littlefs, &file, "#T;"));	// Thermocouples
 	for(uint8_t TKidx =0; TKidx<MAX6675_DEVICES;TKidx++)
 	{
 		utoa(Temp_TypeK[TKidx],LogWriteBfr,10);
-		EvalLFSError(LFS_WRITE_STRING(littlefs, file, LogWriteBfr));
-		EvalLFSError(LFS_WRITE_STRING(littlefs, file, ";"));
+		EvalLFSError(LFS_WRITE_STRING(&littlefs, &file, LogWriteBfr));
+		EvalLFSError(LFS_WRITE_STRING(&littlefs, &file, ";"));
 	}
-	EvalLFSError(LFS_WRITE_STRING(littlefs, file, "\n"));
+	EvalLFSError(LFS_WRITE_STRING(&littlefs, &file, "\n"));
 
 	if(LFS_ret <0){
 		GlobalWriteErrorFlag =1;
@@ -643,6 +586,8 @@ void SampleClockHandler(void)
 
 	return;
 }
+
+
 
 
 
