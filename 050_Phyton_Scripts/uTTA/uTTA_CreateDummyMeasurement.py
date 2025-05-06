2b050d3cd36cbef8b73c3a37509d05c53e780b9e
wtronics <w.m.d.koch@googlemail.com>
2025-05-06 21:48:47 +0200

    Update uTTA_CreateDummyMeasurement.py

--- 050_Phyton_Scripts/uTTA/uTTA_CreateDummyMeasurement.py
+++ 050_Phyton_Scripts/uTTA/uTTA_CreateDummyMeasurement.py
@@ -18,11 +18,20 @@ SampDecade = 250
 SimulatedEndTime = 7200
 
 # Simulated foster RC-model
-# Rth_Foster = np.array([0.4,     1.5,   3.5,  4.0, 5.5, 5.7], dtype=float)
-# Cth_Foster = np.array([0.00055, 0.006, 0.05, 0.3, 1.4, 15.0], dtype=float)
+Rth_Foster = np.array([0.4,     1.5,   3.5,  4.0, 5.5, 5.7], dtype=float)
+Cth_Foster = np.array([0.00055, 0.006, 0.05, 0.3, 1.4, 15.0], dtype=float)
 
 Rth_Foster = np.array([0.4,   5.7], dtype=float)
 Cth_Foster = np.array([0.00055,  15.0], dtype=float)
+
+Rth_Foster = np.array([2, 2.2, 4.5, 6, 21, 24], dtype=float)    # VN9D30Q100 CH1 FP
+Cth_Foster = np.array([0.0005, 0.01, 0.03, 0.3, 1, 2.4], dtype=float)
+
+#Rth_Foster = np.array([2, 2.2, 1,  4.5, 6, 21, 24], dtype=float)    # VN9D30Q100 CH1 FP + dummy
+#Cth_Foster = np.array([0.0005, 0.01, 0.01, 0.03, 0.3, 1, 2.4], dtype=float)
+
+#Rth_Foster = np.array([1], dtype=float)    # Dummy 1tau element
+#Cth_Foster = np.array([1], dtype=float)
 
 # Create Timebase for all measurements
 BlockIdx = 0
@@ -40,9 +49,10 @@ while True:
 Timebase = Timebase / 1000000.0
 Zth = np.zeros(shape=Timebase.shape)
 # run through the RC-element array and create the RC-charging curve
+print("R [K/W];C [Ws/K];tau [s];ln_tau [ln(s)]")
 for Rank in range(0, len(Cth_Foster)):
     Zth = Zth + Rth_Foster[Rank] * (1-np.exp(-Timebase/(Rth_Foster[Rank] * Cth_Foster[Rank])))
-    print("Created RC-Element with R {Rth}K/W and C {Cth}Ws/K tau {Ctau} and ln_tau {lntau}".format(Rth=Rth_Foster[Rank], Cth=Cth_Foster[Rank],
+    print("{Rth};{Cth};{Ctau:.5e};{lntau:.3f}".format(Rth=Rth_Foster[Rank], Cth=Cth_Foster[Rank],
                                                                                                     Ctau=Rth_Foster[Rank]*Cth_Foster[Rank],
                                                                                                     lntau=np.log(Rth_Foster[Rank]*Cth_Foster[Rank])))
 
