1a3558376cc14f5b9a6b1fa293002f1430938c4e
wtronics <w.m.d.koch@googlemail.com>
2025-05-13 22:23:38 +0200

    Create uTTA_umf_Viewer_GUI.py

---
+++ 050_Phyton_Scripts/uTTA/uTTA_umf_Viewer_GUI.py
@@ -0,0 +1,220 @@
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)  # matplotlib 3.9.2
+from matplotlib.figure import Figure
+import tkinter as tk
+from tkinter import messagebox  # part of python 3.12.5
+from tksheet import (  # tksheet 7.2.16
+    Sheet, float_formatter)
+from quantiphy import Quantity      # quantiphy 2.20
+import uTTA_data_import
+import uTTA_data_processing
+import numpy as np                  # numpy 2.1.1
+import numpy.dtypes                 # numpy 2.1.0
+import matplotlib                   # matplotlib 3.9.2
+import customtkinter                # customtkinter 5.2.2
+import customtkinter as ctk         # customtkinter 5.2.2
+
+matplotlib.use("TkAgg")
+
+CalData_FileName = ''
+TimeBaseTotal = []
+ADC = []
+Temp = []
+
+DataFile = ''
+G_Plots = []
+FileOpened = False
+MaxJUT_Channels = 3
+NoOfDUTs = 3
+MetaData = {}
+
+
+class umf_Viewer_App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        global G_Plots
+
+        self.geometry("1480x850")
+        screen_dpi = self.winfo_fpixels('1i')
+        ctk.deactivate_automatic_dpi_awareness()
+        print("DPI: " + str(screen_dpi))
+        self.minsize(1480, 850)
+        self.title("uTTA umf Viewer Tool")
+        self.update()
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)      # window closing event
+        self.grid_columnconfigure(6, weight=1)
+        self.grid_rowconfigure(20, weight=1)
+
+        self.frm_file_btns = ctk.CTkFrame(master=self)
+        self.frm_file_btns.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        self.frm_file_btns.grid_rowconfigure(1, weight=1)
+        self.frm_file_btns.grid_columnconfigure(21, weight=1)
+
+        self.btn_measure_file = ctk.CTkButton(master=self.frm_file_btns,
+                                              text="Measurement File", fg_color="lightblue",
+                                              command=self.read_measurement_file_callback,
+                                              text_color="black")
+        self.btn_measure_file.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+
+        self.frm_meas_data = ctk.CTkFrame(master=self)
+        self.frm_meas_data.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+
+
+        self.frm_help_bar = ctk.CTkFrame(master=self)
+        self.frm_help_bar.grid(row=0, column=1, columnspan=5, padx=10, pady=10, sticky="ew")
+        self.frm_help_bar.grid_rowconfigure(1, weight=1)
+        self.frm_help_bar.grid_columnconfigure(1, weight=1)
+
+        self.lbl_helpbar = ctk.CTkLabel(master=self.frm_help_bar,
+                                        fg_color="lightblue", width=1050, anchor="w", padx=10, text_color="black")
+        self.lbl_helpbar.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        self.lbl_helpbar.configure(text="Welcome to the umf-Viewer GUI. Click 'Measurement File' and import a measurement")
+
+        self.fig = Figure(figsize=(12, 7.5), dpi=screen_dpi)
+        #self.fig.tight_layout()
+        self.fig.subplots_adjust(left=0.06, bottom=0.075, right=0.97, top=0.96, wspace=0.212, hspace=0.42)
+        G_Plots = self.fig.subplots(3, 2)
+
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=1, column=1, rowspan=18, columnspan=5, padx=10, pady=10, sticky="ew")
+        # Toolbar #########
+        toolbar_frame = ctk.CTkFrame(master=self)
+        toolbar_frame.grid(row=20, column=1, columnspan=5)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, toolbar_frame)
+        self.toolbar.update()
+
+        self.update_plots()
+        self.update()
+
+    def update_plots(self):
+
+        global G_Plots, Temp, MetaData, FileOpened
+        if FileOpened:
+            G_Plots[0, 0].clear()
+
+            # Calculate the average ambient temperature as starting point
+            StartTempTC = np.mean(Temp[3, 0:10])
+            print("Averaged start temperature form TC-Channel 3: {Tstart:.3f}°C".format(Tstart=StartTempTC))
+
+            CoolingStartBlock = MetaData["CoolingStartBlock"]
+            SampDecade = MetaData["SamplesPerDecade"]
+
+            # Cut Timebase and measurement to cooling section
+            TimeBase_Cooling, ADC_Cooling = uTTA_data_processing.calculate_cooling_curve(TimeBaseTotal, ADC, CoolingStartBlock, SampDecade)
+
+            # # Calculate the average heating current, voltage and power through the diode
+            PDio_Heat = uTTA_data_processing.calculate_diode_heating(TimeBaseTotal, ADC, CoolingStartBlock, SampDecade)
+
+            UDio_Cold_Start = np.zeros((NoOfDUTs,), numpy.float32)
+            UDio_Cold_End = np.zeros((NoOfDUTs,), numpy.float32)
+            T_Monitor_Heated = np.zeros((NoOfDUTs,), numpy.float32)
+            TDio = np.zeros(shape=ADC_Cooling.shape)
+            for Ch in range(0, NoOfDUTs):
+                ch_tsp = "TSP{Ch}".format(Ch=Ch)
+                if MetaData[ch_tsp]["Name"] != "OFF":
+                    # Calculate the average Diode voltage at the start of the measurement
+                    # print ("Channel {Chan}, Min ADC: {MinADC}, Max ADC: {MaxADC}".format(Chan=Ch,MinADC=ADC[Ch, :].min(), MaxADC=ADC[Ch, :].max()))
+                    UDio_Cold_Start[Ch] = np.mean(ADC[Ch, 0:SampDecade])
+                    # Calculate the average Diode voltage at the end of the measurement
+                    UDio_Cold_End[Ch] = np.mean(ADC[Ch, -SampDecade:-1])
+                    # The TSP Offset is the average diode start voltage
+                    MetaData[ch_tsp]["Offset"] = -UDio_Cold_Start[Ch]
+                    TDio[Ch, :] = (ADC_Cooling[Ch, :] + MetaData[ch_tsp]["Offset"]) / MetaData[ch_tsp]["LinGain"]
+
+                    # Calculate the start temperature of both monitoring channels to have a good starting point for Zth-Matrix
+                    T_Monitor_Heated[Ch] = (np.mean(ADC[Ch, ((CoolingStartBlock - 2) * SampDecade):((CoolingStartBlock - 1) * SampDecade) - 1])
+                                            + MetaData[ch_tsp]["Offset"]) / MetaData[ch_tsp]["LinGain"]
+                    if Ch == 0:
+                        print(
+                            "COLD VOLTAGE: DUT{DUTno} at Start: {Ucold: 3.4f}V; at End: {UColdEnd: 3.4f}V; Delta U: {dU_DUT: 3.4f}V; Delta T: {dT_DUT: 3.4f}°C".format(
+                                DUTno=Ch,
+                                Ucold=UDio_Cold_Start[Ch],
+                                UColdEnd=UDio_Cold_End[Ch],
+                                dU_DUT=UDio_Cold_Start[Ch] - UDio_Cold_End[Ch],
+                                dT_DUT=((UDio_Cold_End[Ch] - UDio_Cold_Start[Ch]) / MetaData[ch_tsp]["LinGain"])))
+                    else:
+                        print("COLD VOLTAGE: DUT{DUTno} at Start: {Ucold: 3.4f}V; at End: {UColdEnd: 3.4f}V; Delta U: {dU_DUT: 3.4f}V; Delta T: {dT_DUT: 3.4f}°C; "
+                              "Heated Temp: {T_heated: 3.4f}°C".format(DUTno=Ch,
+                                                                       Ucold=UDio_Cold_Start[Ch],
+                                                                       UColdEnd=UDio_Cold_End[Ch],
+                                                                       dU_DUT=UDio_Cold_Start[Ch] - UDio_Cold_End[Ch],
+                                                                       dT_DUT=((UDio_Cold_End[Ch] - UDio_Cold_Start[Ch]) / MetaData[ch_tsp]["LinGain"]),
+                                                                       T_heated=T_Monitor_Heated[Ch]))
+
+            for Ch in range(0, NoOfDUTs):
+                ch_tsp = "TSP{Ch}".format(Ch=Ch)
+                if MetaData[ch_tsp]["Name"] != "OFF":
+                    G_Plots[0, 0].plot(TimeBaseTotal, ADC[Ch, :], label=MetaData[ch_tsp]["Name"])  # Plot some data on the axes.
+
+            G_Plots[0, 0].set_title("Diode Voltages of the full measurement")
+            G_Plots[0, 0].set_ylabel('Diode Voltage / [V]')
+            G_Plots[0, 0].set_xlabel('Time / [s]')
+            G_Plots[0, 0].legend()
+            G_Plots[0, 0].grid(which='both')
+
+            G_Plots[0, 1].plot(TimeBaseTotal, ADC[3, :], label="Current")  # Plot some data on the axes.
+            G_Plots[0, 1].set_title("Drive current")
+            G_Plots[0, 1].set_ylabel('Current / [A]')
+            G_Plots[0, 1].set_xlabel('Time / [s]')
+            G_Plots[0, 1].legend()
+            G_Plots[0, 1].grid(which='both')
+
+            for Ch in range(0, NoOfDUTs):
+                ch_tsp = "TSP{Ch}".format(Ch=Ch)
+                if MetaData[ch_tsp]["Name"] != "OFF":
+                    G_Plots[1, 0].semilogx(TimeBase_Cooling, ADC_Cooling[Ch, :], label=MetaData[ch_tsp]["Name"])  # Plot some data on the axes.
+
+            G_Plots[1, 0].set_title("Diode Voltages of the cooling section")
+            G_Plots[1, 0].set_ylabel('Diode Voltage / [V]')
+            G_Plots[1, 0].set_xlabel('Time / [s]')
+            G_Plots[1, 0].legend()
+            G_Plots[1, 0].grid(which='both')
+
+            G_Plots[1, 1].semilogx(TimeBase_Cooling, ADC_Cooling[3, :], label="Current")  # Plot some data on the axes.
+            G_Plots[1, 1].set_title("Drive current in cooling section")
+            G_Plots[1, 1].set_ylabel('Current / [A]')
+            G_Plots[1, 1].set_xlabel('Time / [s]')
+            G_Plots[1, 1].legend()
+            G_Plots[1, 1].grid(which='both')
+
+            G_Plots[2, 0].set_title("Thermocouple data of the full measurement")
+            G_Plots[2, 0].plot(Temp[0, :], label="Sensor 1")  # Plot some data on the axes.
+            G_Plots[2, 0].plot(Temp[1, :], label="Sensor 2")  # Plot some data on the axes.
+            G_Plots[2, 0].plot(Temp[2, :], label="Sensor 3")  # Plot some data on the axes.
+            G_Plots[2, 0].plot(Temp[3, :], label="Sensor 4")  # Plot some data on the axes.
+            G_Plots[2, 0].set_ylabel('Temperature / [°C]')
+            G_Plots[2, 0].set_xlabel('Sample')
+            G_Plots[2, 0].legend()
+            G_Plots[2, 0].grid(which='both')
+
+            self.canvas.draw()
+
+    def read_measurement_file_callback(self):
+        global TimeBaseTotal, ADC, Temp, DataFile, MetaData, FileOpened
+        measfilename = uTTA_data_import.select_file("Select the measurement file",
+                                                    (('uTTA Measurement Files', '*.umf'), ('Text-Files', '*.txt'), ('All files', '*.*')))
+        if len(measfilename) > 0:    # check if string is not empty
+            DataFile, data_file_no_ext, file_path = uTTA_data_import.split_file_path(measfilename)
+
+            TimeBaseTotal, ADC, Temp, MetaData = uTTA_data_import.read_measurement_file(measfilename, 0)
+            FileOpened = True
+            self.update_plots()
+
+            self.lbl_helpbar.configure(text="File: " + DataFile + " was successfully imported. Now click on the magnifying glass below the plot and select "+
+                                       "the first horizontal section in the upper plot.")
+
+        else:
+            self.lbl_helpbar.configure(text="File: " + DataFile + " was not imported.")
+
+    def on_closing(self):
+        # if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        self.destroy()
+
+
+# Declare and register callbacks
+
+app = umf_Viewer_App()
+
+app.mainloop()
