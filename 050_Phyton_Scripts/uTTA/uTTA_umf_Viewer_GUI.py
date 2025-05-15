3bdd41a2a583360dd91e7669ab8b01a9f1580135
wtronics <w.m.d.koch@googlemail.com>
2025-05-15 22:17:05 +0200

    Update uTTA_umf_Viewer_GUI.py
    Changed from customtkinter to ttkbootstrap for better styling
    removed unused dependencies

--- 050_Phyton_Scripts/uTTA/uTTA_umf_Viewer_GUI.py
+++ 050_Phyton_Scripts/uTTA/uTTA_umf_Viewer_GUI.py
@@ -1,17 +1,11 @@
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)  # matplotlib 3.9.2
 from matplotlib.figure import Figure
-import tkinter as tk
-from tkinter import messagebox  # part of python 3.12.5
-from tksheet import (  # tksheet 7.2.16
-    Sheet, float_formatter)
-from quantiphy import Quantity      # quantiphy 2.20
 import uTTA_data_import
 import uTTA_data_processing
 import numpy as np                  # numpy 2.1.1
 import numpy.dtypes                 # numpy 2.1.0
 import matplotlib                   # matplotlib 3.9.2
-import customtkinter                # customtkinter 5.2.2
-import customtkinter as ctk         # customtkinter 5.2.2
+import ttkbootstrap as ttk          # ttkbootstrap 1.13.5
 
 matplotlib.use("TkAgg")
 
@@ -28,63 +22,58 @@ NoOfDUTs = 3
 MetaData = {}
 
 
-class umf_Viewer_App(customtkinter.CTk):
+class umf_viewer_App(ttk.Window):
     def __init__(self):
         super().__init__()
+        self.title("uTTA umf-Viewver")
+        self.geometry("1480x900")
+        self.minsize(1480, 900)
+        screen_dpi = self.winfo_fpixels('1i')
+#        self.protocol("WM_DELETE_WINDOW", self.on_closing)      # window closing event
 
+        # widgets
         global G_Plots
 
-        self.geometry("1480x850")
-        screen_dpi = self.winfo_fpixels('1i')
-        ctk.deactivate_automatic_dpi_awareness()
-        print("DPI: " + str(screen_dpi))
-        self.minsize(1480, 850)
-        self.title("uTTA umf Viewer Tool")
-        self.update()
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)      # window closing event
         self.grid_columnconfigure(6, weight=1)
         self.grid_rowconfigure(20, weight=1)
 
-        self.frm_file_btns = ctk.CTkFrame(master=self)
+        self.frm_file_btns = ttk.Frame(master=self)
         self.frm_file_btns.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
         self.frm_file_btns.grid_rowconfigure(1, weight=1)
         self.frm_file_btns.grid_columnconfigure(21, weight=1)
 
-        self.btn_measure_file = ctk.CTkButton(master=self.frm_file_btns,
-                                              text="Measurement File", fg_color="lightblue",
-                                              command=self.read_measurement_file_callback,
-                                              text_color="black")
+        self.btn_measure_file = ttk.Button(master=self.frm_file_btns, text="Measurement File",
+                                           command=self.read_measurement_file_callback, bootstyle="dark")
         self.btn_measure_file.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-        self.frm_meas_data = ctk.CTkFrame(master=self)
+        self.frm_meas_data = ttk.Frame(master=self)
         self.frm_meas_data.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        self.frm_meas_data.grid_rowconfigure(index=6, weight=1)
+        self.frm_meas_data.grid_columnconfigure(index=1, weight=1)
 
-
-        self.frm_help_bar = ctk.CTkFrame(master=self)
+        self.frm_help_bar = ttk.Frame(master=self, bootstyle="info")
         self.frm_help_bar.grid(row=0, column=1, columnspan=5, padx=10, pady=10, sticky="ew")
         self.frm_help_bar.grid_rowconfigure(1, weight=1)
         self.frm_help_bar.grid_columnconfigure(1, weight=1)
 
-        self.lbl_helpbar = ctk.CTkLabel(master=self.frm_help_bar,
-                                        fg_color="lightblue", width=1050, anchor="w", padx=10, text_color="black")
+        self.lbl_helpbar = ttk.Label(master=self.frm_help_bar, anchor="w", bootstyle="inverse-info")
         self.lbl_helpbar.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
         self.lbl_helpbar.configure(text="Welcome to the umf-Viewer GUI. Click 'Measurement File' and import a measurement")
 
-        self.fig = Figure(figsize=(12, 7.5), dpi=screen_dpi)
-        #self.fig.tight_layout()
-        self.fig.subplots_adjust(left=0.06, bottom=0.075, right=0.97, top=0.96, wspace=0.212, hspace=0.42)
+        self.frm_plot_area = ttk.Frame(master=self)
+        self.frm_plot_area.grid(row=1, column=1, columnspan=5, padx=10, pady=10, sticky="ew")
+        self.fig = Figure(figsize=(8.4, 4.95), dpi=screen_dpi)
+        self.fig.subplots_adjust(left=0.06, bottom=0.075, right=0.97, top=0.96, wspace=0.212, hspace=0.54)
         G_Plots = self.fig.subplots(3, 2)
 
-
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frm_plot_area)
         self.canvas.draw()
         self.canvas.get_tk_widget().grid(row=1, column=1, rowspan=18, columnspan=5, padx=10, pady=10, sticky="ew")
         # Toolbar #########
-        toolbar_frame = ctk.CTkFrame(master=self)
-        toolbar_frame.grid(row=20, column=1, columnspan=5)
+        toolbar_frame = ttk.Frame(master=self)
+        toolbar_frame.grid(row=21, column=1, columnspan=5)
         self.toolbar = NavigationToolbar2Tk(self.canvas, toolbar_frame)
         self.toolbar.update()
-
         self.update_plots()
         self.update()
 
@@ -148,17 +137,19 @@ class umf_Viewer_App(customtkinter.CTk):
                 if MetaData[ch_tsp]["Name"] != "OFF":
                     G_Plots[0, 0].plot(TimeBaseTotal, ADC[Ch, :], label=MetaData[ch_tsp]["Name"])  # Plot some data on the axes.
 
-            G_Plots[0, 0].set_title("Diode Voltages of the full measurement")
-            G_Plots[0, 0].set_ylabel('Diode Voltage / [V]')
-            G_Plots[0, 0].set_xlabel('Time / [s]')
-            G_Plots[0, 0].legend()
+            G_Plots[0, 0].set_title("Diode Voltages of the full measurement", size=8)
+            G_Plots[0, 0].set_ylabel('Diode Voltage / [V]', size=8)
+            G_Plots[0, 0].set_xlabel('Time / [s]', size=8)
+            G_Plots[0, 0].tick_params(axis='both', which='major', labelsize=7)
+            G_Plots[0, 0].legend(prop={'size': 6})
             G_Plots[0, 0].grid(which='both')
 
             G_Plots[0, 1].plot(TimeBaseTotal, ADC[3, :], label="Current")  # Plot some data on the axes.
-            G_Plots[0, 1].set_title("Drive current")
-            G_Plots[0, 1].set_ylabel('Current / [A]')
-            G_Plots[0, 1].set_xlabel('Time / [s]')
-            G_Plots[0, 1].legend()
+            G_Plots[0, 1].set_title("Drive current", size=8)
+            G_Plots[0, 1].tick_params(axis='both', which='major', labelsize=7)
+            G_Plots[0, 1].set_ylabel('Current / [A]', size=8)
+            G_Plots[0, 1].set_xlabel('Time / [s]', size=8)
+            G_Plots[0, 1].legend(prop={'size': 6})
             G_Plots[0, 1].grid(which='both')
 
             for Ch in range(0, NoOfDUTs):
@@ -166,30 +157,34 @@ class umf_Viewer_App(customtkinter.CTk):
                 if MetaData[ch_tsp]["Name"] != "OFF":
                     G_Plots[1, 0].semilogx(TimeBase_Cooling, ADC_Cooling[Ch, :], label=MetaData[ch_tsp]["Name"])  # Plot some data on the axes.
 
-            G_Plots[1, 0].set_title("Diode Voltages of the cooling section")
-            G_Plots[1, 0].set_ylabel('Diode Voltage / [V]')
-            G_Plots[1, 0].set_xlabel('Time / [s]')
-            G_Plots[1, 0].legend()
+            G_Plots[1, 0].set_title("Diode Voltages of the cooling section", size=8)
+            G_Plots[1, 0].set_ylabel('Diode Voltage / [V]', size=8)
+            G_Plots[1, 0].set_xlabel('Time / [s]', size=8)
+            G_Plots[1, 0].tick_params(axis='both', which='major', labelsize=7)
+            G_Plots[1, 0].legend(prop={'size': 6})
             G_Plots[1, 0].grid(which='both')
 
             G_Plots[1, 1].semilogx(TimeBase_Cooling, ADC_Cooling[3, :], label="Current")  # Plot some data on the axes.
-            G_Plots[1, 1].set_title("Drive current in cooling section")
-            G_Plots[1, 1].set_ylabel('Current / [A]')
-            G_Plots[1, 1].set_xlabel('Time / [s]')
-            G_Plots[1, 1].legend()
+            G_Plots[1, 1].set_title("Drive current in cooling section", size=8)
+            G_Plots[1, 1].set_ylabel('Current / [A]', size=8)
+            G_Plots[1, 1].set_xlabel('Time / [s]', size=8)
+            G_Plots[1, 1].tick_params(axis='both', which='major', labelsize=7)
+            G_Plots[1, 1].legend(prop={'size': 6})
             G_Plots[1, 1].grid(which='both')
 
-            G_Plots[2, 0].set_title("Thermocouple data of the full measurement")
+            G_Plots[2, 0].set_title("Thermocouple data of the full measurement", size=8)
             G_Plots[2, 0].plot(Temp[0, :], label="Sensor 1")  # Plot some data on the axes.
             G_Plots[2, 0].plot(Temp[1, :], label="Sensor 2")  # Plot some data on the axes.
             G_Plots[2, 0].plot(Temp[2, :], label="Sensor 3")  # Plot some data on the axes.
             G_Plots[2, 0].plot(Temp[3, :], label="Sensor 4")  # Plot some data on the axes.
-            G_Plots[2, 0].set_ylabel('Temperature / [°C]')
-            G_Plots[2, 0].set_xlabel('Sample')
-            G_Plots[2, 0].legend()
+            G_Plots[2, 0].set_ylabel('Temperature / [°C]', size=8)
+            G_Plots[2, 0].set_xlabel('Sample', size=8)
+            G_Plots[2, 0].tick_params(axis='both', which='major', labelsize=7)
+            G_Plots[2, 0].legend(prop={'size': 6})
             G_Plots[2, 0].grid(which='both')
 
             self.canvas.draw()
+
 
     def read_measurement_file_callback(self):
         global TimeBaseTotal, ADC, Temp, DataFile, MetaData, FileOpened
@@ -202,19 +197,21 @@ class umf_Viewer_App(customtkinter.CTk):
             FileOpened = True
             self.update_plots()
 
+
             self.lbl_helpbar.configure(text="File: " + DataFile + " was successfully imported. Now click on the magnifying glass below the plot and select "+
-                                       "the first horizontal section in the upper plot.")
+                                       "the first horizontal section in the upper plot.", bootstyle="inverse-success")
+            self.frm_help_bar.configure(bootstyle="success")
 
         else:
-            self.lbl_helpbar.configure(text="File: " + DataFile + " was not imported.")
-
-    def on_closing(self):
-        # if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        self.destroy()
+            self.lbl_helpbar.configure(text="File: " + DataFile + " was not imported.", bootstyle="inverse-danger")
+            self.frm_help_bar.configure(bootstyle="danger")
 
 
-# Declare and register callbacks
+def on_closing(self):
+    # if messagebox.askokcancel("Quit", "Do you want to quit?"):
+    self.destroy()
 
-app = umf_Viewer_App()
+
+app = umf_viewer_App()
 
 app.mainloop()
