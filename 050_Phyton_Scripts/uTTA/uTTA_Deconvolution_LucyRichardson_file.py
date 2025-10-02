3aed30aca6eb101e159c0a8149b9908bc47a2ff2
wtronics <w.m.d.koch@googlemail.com>
2025-10-02 20:02:32 +0200

    Fixed library dependency

--- 050_Phyton_Scripts/uTTA/uTTA_Deconvolution_LucyRichardson_file.py
+++ 050_Phyton_Scripts/uTTA/uTTA_Deconvolution_LucyRichardson_file.py
@@ -1,15 +1,16 @@
 from numpy import genfromtxt                    # numpy 2.1.0
-import uTTA_data_processing as udpc
+import library.uTTA_Deconvolution as ud_deconv
+import library.uTTA_data_processing as udi
 import time                                     # part of python 3.12.5
 
 
 FileNam = ""
 
-# FileNam = r"C:\temp\Eigenentwicklungen\GitHub\uTTA\060_Example_Measurement_Data\Simulated_Measurement_1RC.t3i"
+# FileNam = os.path.abspath(r"..\..\060_Example_Measurement_Data\Simulated_Measurement_1RC.t3i")
 if len(FileNam) < 5:
-    FileNam = udpc.select_file('Open a t3i-File', (('T3I Measurement Files', '*.t3i'), ('All files', '*.*')))
+    FileNam = udi.select_file('Open a t3i-File', (('T3I Measurement Files', '*.t3i'), ('All files', '*.*')))
 
-DataFile, DataFileNoExt, FilePath = udpc.split_file_path(FileNam)
+DataFile, DataFileNoExt, FilePath = udi.split_file_path(FileNam)
 
 start = time.time()
 
@@ -17,7 +18,7 @@ Zth_Import = genfromtxt(FileNam, delimiter='\t', dtype=float, names=True)
 Cols = Zth_Import.dtype.names
 
 if len(Cols) > 1:
-    deconv = udpc.UttaDeconvolution()
+    deconv = ud_deconv.UttaDeconvolution()
     deconv.import_zth_input_data(Zth_Import[Cols[0]] , Zth_Import[Cols[1]])
 
     deconv.prepare_zth_deconvolution()
