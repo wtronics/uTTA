a3295211e345e0ca56eabe2c1da25f5f6c97326d
wtronics <w.m.d.koch@googlemail.com>
2026-03-18 22:07:10 +0100

    Added Logging support and Documentation

--- 050_Phyton_Scripts/uTTA/library/uTTA_Deconvolution.py
+++ 050_Phyton_Scripts/uTTA/library/uTTA_Deconvolution.py
@@ -1,14 +1,22 @@
-import numpy as np              # numpy 2.1.0
+import numpy as np
 from scipy.signal import find_peaks
-import matplotlib.pyplot as plt                 # matplotlib 3.9.2
-from skimage import restoration    # scikit-image 0.24.0
+from skimage import restoration    # scikit-image
 import library.uTTA_data_processing as udProc
 import library.uTTA_data_plotting as ud_plot
+import logging
 
 
 class UttaDeconvolution:
 
-    def __init__(self):
+    def __init__(self, logger=None):
+
+        if logger is None:
+            self.logger = logging.getLogger("dummy")
+            self.logger.addHandler(logging.NullHandler())
+            self.logger.propagate = False # Important: prevents forwarding to the root logger
+        else:
+            self.logger = logger
+
         # Variables for the Zth deconvolution with Lucy-Richardson Algorithm
         self.rth_stat = 0
         self.deconv_samples_per_decade = 50
@@ -32,26 +40,71 @@ class UttaDeconvolution:
         self.ref_deconv = np.array([])
 
     def import_zth_input_data(self, timebase, zth):
+        """
+        Import zth-data and their timebase into the class from two independent arrays of the same length
+
+        Args:
+            timebase (list oder numpy.array): timepoints when the zth samples were taken
+            zth      (list oder numpy.array): thermal impedances at a given time
+
+        Returns:
+            None
+                
+        Raises:
+            ValueError: When array length are not the same
+        """
+
+        if len(timebase) != len(zth):
+            raise ValueError("Array length must be the same")
         self.z_raw = np.log(timebase)
         self.zth_raw = zth
     
     def import_from_postprocess(self, utta_postprocess:udProc.UttaZthProcessing):
+        """
+        Imports zth-data and the timebase into the classs for processing
+
+        Args:
+            utta_postprocess (UttaZthProcessing): uTTA Postprocessing class
+
+        Returns:
+            None
+                
+        Raises:
+            ValueError: When postprocessing class has no data to hand over
+        """
+
+        if not utta_postprocess.flag_import_successful:
+            raise ValueError("No data to load from UttaZthProcessing")
         self.z_raw = np.log(utta_postprocess.adc_timebase_cooling)
         self.zth_raw = utta_postprocess.zth[0, :]
 
     def prepare_zth_deconvolution(self):
+        """
+        Prepares imported data for the upcoming deconvolution:
+            - prepares a new, evenly spaced timebase in ln(t)-space
+            - do the differentiation (da/dz) of the input curve
+            - create the reference function (greens function) for the deconvolution
 
+        Args:
+            None
+
+        Returns:
+            None
+                
+        Raises:
+            None
+        """
         no_sample_points = int((self.z_raw[-1] - self.z_raw[0]) * self.deconv_samples_per_decade)
-        print(f"Number of SamplePoints: {no_sample_points}")
+        self.logger.info(f"Number of SamplePoints: {no_sample_points}")
 
         log_samp_point_intervall = (self.z_raw[-1] - self.z_raw[0]) / no_sample_points
-        print(f"SamplePoint Intervall: {log_samp_point_intervall}")
+        self.logger.info(f"SamplePoint Intervall: {log_samp_point_intervall}")
 
         z = np.linspace(start=self.z_raw[0], stop=self.z_raw[-1], num=no_sample_points) # create constant step width timebase
         self.a_z = np.interp(z, self.z_raw, self.zth_raw)
 
         self.rth_stat = np.mean(self.a_z[int(0.98 * len(self.a_z)):-1])
-        print(f"Zth end value: {self.rth_stat:.3f}, Zth Curve Start Value {self.zth_raw[0]}K/W")
+        self.logger.info(f"Zth end value: {self.rth_stat:.3f}, Zth Curve Start Value {self.zth_raw[0]}K/W")
 
         self.dadz = np.diff(self.a_z) / np.diff(z)
         self.w_z = np.exp(z - np.exp(z))
@@ -60,28 +113,55 @@ class UttaDeconvolution:
         return
 
     def deconvolve_get_peaks(self, dadz, w_z, iterations):
+        """
+        Deconvolve the prepared signal by using the Lucy-Richardson deconvolution algorithm.
+        After deconvolution, try to find the visible peaks as a first attempt to identify the equivalent RC-elements
 
+        Args:
+            None
+
+        Returns:
+            None
+                
+        Raises:
+            None
+        """
         deconv = restoration.richardson_lucy(dadz, self.w_z[:-1] / np.sum(w_z[:-1]),
                                              num_iter=iterations,
                                              clip=False)  # * sum_ref_dadz
         peaks, _ = find_peaks(deconv)
         return deconv, peaks
+    
+    def deconvolve_zth_lucy_richardson(self, iterations):
+        self.deconvolved, self.peaks = self.deconvolve_get_peaks(self.dadz, self.w_z, iterations)
 
-    def add_reference_deconv_output_plot(self):
-        iterations = 1000
-        lines = [
-                {'x_data': self.z[:-1],
-                 'y_data': self.ref_deconv,
-                 'label': f"Reference deconv with {iterations} Iterations",
-                 'axis': 0}]
+        self.dadz_deconvolved = (np.convolve(self.deconvolved, self.w_z, "same")) * (self.z[2] - self.z[1])
+        self.zth_deconvolved = (np.cumsum(self.dadz_deconvolved) / np.sum(self.deconvolved)) * self.rth_stat + self.a_z[0]
+        return
 
-        return ud_plot.UttaPlotConfiguration(plot_type='line',
-                                             data=lines,
-                                             x_label='Tau / [s]',
-                                             y_label='A.U',
-                                             title='Reference Deconvolution (1K/W & 1Ws/K)')
+    def report_peaks_to_console(self, peaks):
+        outp = ""
+        print(f"Deconvolvable to {len(peaks)} peaks")
+        for peak in peaks:
+            outp = outp + f"{self.z[peak]:.4e};{self.deconvolved[peak]:.4e};"
+        print(outp)
+        return
 
-    def add_deconv_output_plot(self):
+    # def add_reference_deconv_output_plot(self):
+    #     iterations = 1000
+    #     lines = [
+    #             {'x_data': self.z[:-1],
+    #              'y_data': self.ref_deconv,
+    #              'label': f"Reference deconv with {iterations} Iterations",
+    #              'axis': 0}]
+
+    #     return ud_plot.UttaPlotConfiguration(plot_type='line',
+    #                                          data=lines,
+    #                                          x_label='Tau / [s]',
+    #                                          y_label='A.U',
+    #                                          title='Reference Deconvolution (1K/W & 1Ws/K)')
+
+    def add_deconv_tau_output_plot(self):
         iterations = 1000
         lines = [
                 {'x_data': self.z[:-1],
@@ -95,33 +175,33 @@ class UttaDeconvolution:
                                              y_label='Zth / [K/W]',
                                              title='Deconvolved Spectrum')
 
-    def add_dadz_deconv_output_plot(self):
-        iterations = 1000
-        lines = [
-                {'x_data': self.z,
-                 'y_data': self.dadz_deconvolved,
-                 'label': f"da/dz {iterations} Iterations",
-                 'axis': 0}]
+    # def add_dadz_deconv_output_plot(self):
+    #     iterations = 1000
+    #     lines = [
+    #             {'x_data': self.z,
+    #              'y_data': self.dadz_deconvolved,
+    #              'label': f"da/dz {iterations} Iterations",
+    #              'axis': 0}]
 
-        return ud_plot.UttaPlotConfiguration(plot_type='line',
-                                             data=lines,
-                                             x_label='Tau / [s]',
-                                             y_label='A.U',
-                                             title='da/dz')
+    #     return ud_plot.UttaPlotConfiguration(plot_type='line',
+    #                                          data=lines,
+    #                                          x_label='Tau / [s]',
+    #                                          y_label='A.U',
+    #                                          title='da/dz')
 
-    def add_dadz_deconv_error_plot(self):
-        iterations = 1000
-        lines = [
-                {'x_data': self.z[:-1],
-                 'y_data': self.dadz - self.dadz_deconvolved[:-1],
-                 'label': f"Delta da/dz {iterations} Iterations",
-                 'axis': 0}]
+    # def add_dadz_deconv_error_plot(self):
+    #     iterations = 1000
+    #     lines = [
+    #             {'x_data': self.z[:-1],
+    #              'y_data': self.dadz - self.dadz_deconvolved[:-1],
+    #              'label': f"Delta da/dz {iterations} Iterations",
+    #              'axis': 0}]
 
-        return ud_plot.UttaPlotConfiguration(plot_type='line',
-                                             data=lines,
-                                             x_label='Tau / [s]',
-                                             y_label='A.U',
-                                             title='da/dz Error')
+    #     return ud_plot.UttaPlotConfiguration(plot_type='line',
+    #                                          data=lines,
+    #                                          x_label='Tau / [s]',
+    #                                          y_label='A.U',
+    #                                          title='da/dz Error')
  
     def add_zth_deconvolution_error_plot(self):
         iterations = 1000
@@ -156,18 +236,5 @@ class UttaDeconvolution:
                                              y_label='Thermal Impedance / [K/W]',
                                              title='Thermal Impedance')
 
-    def deconvolve_zth_lucy_richardson(self, iterations):
-        self.deconvolved, self.peaks = self.deconvolve_get_peaks(self.dadz, self.w_z, iterations)
 
-        self.dadz_deconvolved = (np.convolve(self.deconvolved, self.w_z, "same")) * (self.z[2] - self.z[1])
-        self.zth_deconvolved = (np.cumsum(self.dadz_deconvolved) / np.sum(self.deconvolved)) * self.rth_stat + self.a_z[0]
-        return
-
-    def report_peaks_to_console(self, peaks):
-        outp = ""
-        print(f"Deconvolvable to {len(peaks)} peaks")
-        for peak in peaks:
-            outp = outp + f"{self.z[peak]:.4e};{self.deconvolved[peak]:.4e};"
-        print(outp)
-        return
 
